2efc642505d765a268ecdcc1fa0cf6568efaef73
Озеров Роман <ozrv@inbox.ru>
2026-04-17 19:02:38 +0500

    Сделал footer

--- my-app/src/components/layout/footer/Footer.jsx
+++ my-app/src/components/layout/footer/Footer.jsx
@@ -1,10 +1,71 @@
 import './Footer.css';
 
-function Footer ({children}) {
+function Footer () {
+
+  const search = () => {
+    console.log(`search is on`)
+  }
     return (
-      <>
-        {children}
-      </>
+        <div className='main-footer'>
+          <div className='coloumn-footer'>
+            <p className='footer-title'>
+              About
+            </p>
+            <p className='footer-list'>
+              About Us
+            </p>
+            <p className='footer-list'>
+              Careers
+            </p>
+            <p className='footer-list'>
+              Press
+            </p>
+          </div>
+          <div className='coloumn-footer'>
+            <p className='footer-title'>
+              Support
+            </p>
+            <p className='footer-list'>
+              Contact
+            </p>
+            <p className='footer-list'>
+              FAQ
+            </p>
+            <p className='footer-list'>
+              Shipping
+            </p>
+          </div>
+          <div className='coloumn-footer'>
+            <p className='footer-title'>
+              Legal
+            </p>
+            <p className='footer-list'>
+              Privacy Policy
+            </p>
+            <p className='footer-list'>
+              Terms of Service
+            </p>
+            <p className='footer-list'>
+              Returns
+            </p>
+          </div>
+          <div className='coloumn-footer'>
+            <p className='footer-title'>
+              Newsletter
+            </p>
+            <form action={search} className='subscribe-form'>
+              <label>Subscribe for exclusive deals</label>
+              <div className='input-subscribe-conteiner'>
+                <input 
+                  type='text' 
+                  className='input-email-to-subscribe' 
+                  placeholder='Email'
+                />
+                <button className='sub-button-newsletter'>Subscribe</button>
+              </div>
+            </form>
+          </div>
+        </div>
     )
 }
 
